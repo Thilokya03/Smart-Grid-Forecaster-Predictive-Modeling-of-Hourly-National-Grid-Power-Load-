6ffa0048560da57899f2a67150d50b8df6d71be7
Kusal Nirukshan <nirukshan123@gmail.com>
2026-08-10 15:17:24 +0530

    UK weather data

---
+++ uk_weather_config.py
@@ -0,0 +1,30 @@
+UK_AVERAGE_CITY = "UK_Average"
+TIMEZONE = "Europe/London"
+
+UK_CITIES = {
+    "London": (51.528779434425914, -0.03167830324535154),
+    "Birmingham": (52.485304926781296, -1.887672290688347),
+    "Manchester": (53.48039032597599, -2.2468115254175958),
+    "Bristol": (51.454770262498315, -2.5859138272903635),
+    "Leeds": (53.80110823617203, -1.5566776964780769),
+    "Newcastle": (55.009507351038785, -1.0199556067170723),
+    "Cardiff": (51.484684046491346, -3.161450855621603),
+    "Glasgow": (55.863827953066824, -4.261307828859781),
+    "Edinburgh": (55.95463782474475, -3.1916084211489486),
+    "Inverness": (57.477681317783805, -4.228209847169974),
+}
+
+HOURLY_VARIABLES = [
+    "temperature_2m",
+    "relative_humidity_2m",
+    "dew_point_2m",
+    "apparent_temperature",
+    "precipitation",
+    "rain",
+    "surface_pressure",
+    "cloud_cover",
+    "wind_speed_10m",
+    "wind_direction_10m",
+    "shortwave_radiation",
+]
+
